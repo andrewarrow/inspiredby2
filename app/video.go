15240b93d73a6ab3647a5489683e697a6164f42d
Andrew Arrow <oneone@gmail.com>
2024-08-25 07:39:32 -0700

    merged feature branch

--- app/video.go
+++ app/video.go
@@ -28,60 +28,76 @@ func ProcessVideo(c *router.Context, guid string) {
 
 	sec := int(d)
 	minutes := (sec % 3600) / 60
-	for i := 12; i < minutes+1; i++ {
+	for i := 0; i < minutes+1; i++ {
 		from := 0 + (i * 60)
 		to := from + 10
 		for j := 0; j < 6; j++ {
-			output := fmt.Sprintf("data/%s_%d_%d.mp4", guid, i, j)
-			cmd := exec.Command("ffmpeg", "-i", "data/"+guid+".mp4",
-				"-ss", fmt.Sprintf("%d", from), "-to",
-				fmt.Sprintf("%d", to),
-				"-c:v", "libx264", "-c:a", "aac", "-y",
-				output)
-			fmt.Println(i, j, from, to)
 			sectionId := fmt.Sprintf("%d_%d_%d", one["id"], i, j)
-			cmd.CombinedOutput()
-			c.Params = map[string]any{}
-			c.Params["link_id"] = one["id"]
-			c.Params["section"] = sectionId
-			c.Params["minute"] = i
-			c.Params["sub"] = j
-			c.Params["meta"] = 1
-			c.Insert("link_section")
+			oneSection := c.One("link_section", "where section=$1", sectionId)
+			output := fmt.Sprintf("data/%s_%d_%d.mp4", guid, i, j)
+
+			if len(oneSection) == 0 {
+
+				cmd := exec.Command("ffmpeg", "-i", "data/"+guid+".mp4",
+					"-ss", fmt.Sprintf("%d", from), "-to",
+					fmt.Sprintf("%d", to),
+					"-c:v", "libx264", "-c:a", "aac", "-y",
+					output)
+				fmt.Println(i, j, from, to)
+				cmd.CombinedOutput()
+				c.Params = map[string]any{}
+				c.Params["link_id"] = one["id"]
+				c.Params["section"] = sectionId
+				c.Params["minute"] = i
+				c.Params["sub"] = j
+				c.Params["meta"] = 1
+				c.Insert("link_section")
+			}
+			oneSection = c.One("link_section", "where section=$1", sectionId)
 			//fmt.Println(string(b), err)
 
 			flac := fmt.Sprintf("data/%s_%d_%d.flac", guid, i, j)
-			cmd = exec.Command("ffmpeg", "-i", output,
-				"-b:a", "32k", "-ar", "16000", "-acodec", "flac",
-				"-y",
-				flac)
-			fmt.Println(i, j, from, to)
-			cmd.CombinedOutput()
-			c.FreeFormUpdate("update link_sections set meta=2 where section=$1", sectionId)
+			if oneSection["meta"] == "1" {
+				cmd := exec.Command("ffmpeg", "-i", output,
+					"-b:a", "32k", "-ar", "16000", "-acodec", "flac",
+					"-y",
+					flac)
+				fmt.Println(i, j, from, to)
+				cmd.CombinedOutput()
+				c.FreeFormUpdate("update link_sections set meta=2 where section=$1", sectionId)
+			}
+			oneSection = c.One("link_section", "where section=$1", sectionId)
 
-			stt := google.Speech(flac)
-			c.FreeFormUpdate("update link_sections set meta=3,stt=$1 where section=$2",
-				stt, sectionId)
+			if oneSection["meta"] == "2" {
+				stt := google.Speech(flac)
+				c.FreeFormUpdate("update link_sections set meta=3,stt=$1 where section=$2",
+					stt, sectionId)
+			}
 
 			// ---
 
 			from += 10
 			to += 10
 		}
-		all := c.All("link_section", "where link_id=$1 and minute=$2 order by sub", "",
-			one["id"], i)
-		buffer := []string{}
-		for _, item := range all {
-			buffer = append(buffer, item["stt"].(string))
-		}
-		s := groq.Summarize(strings.Join(buffer, " "))
 		minuteKey := fmt.Sprintf("%d_%d", one["id"], i)
-		c.Params = map[string]any{}
-		c.Params["link_id"] = one["id"]
-		c.Params["minute"] = i
-		c.Params["minute_key"] = minuteKey
-		c.Params["summary"] = s
-		c.Insert("link_minute")
+		oneMinute := c.One("link_minute", "where minute_key=$1", minuteKey)
+
+		if len(oneMinute) == 0 {
+
+			all := c.All("link_section", "where link_id=$1 and minute=$2 order by sub", "",
+				one["id"], i)
+			buffer := []string{}
+			for _, item := range all {
+				buffer = append(buffer, item["stt"].(string))
+			}
+			s := groq.Summarize(strings.Join(buffer, " "))
+			c.Params = map[string]any{}
+			c.Params["link_id"] = one["id"]
+			c.Params["minute"] = i
+			c.Params["minute_key"] = minuteKey
+			c.Params["summary"] = s
+			c.Insert("link_minute")
+		}
 	}
 
 }
