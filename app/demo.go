ae2dca4e950703f30d3a4e0553765d8033c58ce9
Andrew Arrow <oneone@gmail.com>
2024-08-26 10:07:20 -0700

    improved logging

--- app/demo.go
+++ app/demo.go
@@ -3,6 +3,9 @@ package app
 import "github.com/andrewarrow/feedback/router"
 
 func handleDemo(c *router.Context) {
+	link := map[string]any{}
+	link["link"] = "https://youtu.be/wh_M25S2xUw"
 	send := map[string]any{}
+	send["item"] = link
 	c.SendContentInLayout("demo.html", send, 200)
 }
