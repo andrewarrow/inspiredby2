8bf0597f73b64b43bf78eb6deebf31af4566c8b5
Andrew Arrow <oneone@gmail.com>
2024-08-26 11:17:27 -0700

    updated database schema

--- browser/poll.go
+++ browser/poll.go
@@ -29,6 +29,7 @@ func PollForDemoUpdates() {
 		}
 	}
 	Document.Id("thumbs").RemoveClass("hidden")
+	drawEachMinute(55)
 	msg := Document.Id("msg")
 	msg.Set("innerHTML", "Breaking 55 min video into parts.")
 	for {
@@ -41,6 +42,42 @@ func PollForDemoUpdates() {
 			break
 		}
 	}
+}
+
+func drawEachMinute(minutes int) {
+	canvas := Document.Id("canvas")
+	for i := 0; i < minutes+1; i++ {
+		inside := `<div>
+<div>
+minute %d
+</div>
+<div id="sub-%d-0">
+&nbsp;&nbsp;section 1
+</div>
+<div id="sub-%d-1">
+&nbsp;&nbsp;section 2
+</div>
+<div id="sub-%d-2">
+&nbsp;&nbsp;section 3
+</div>
+<div id="sub-%d-3">
+&nbsp;&nbsp;section 4
+</div>
+<div id="sub-%d-4">
+&nbsp;&nbsp;section 5
+</div>
+<div id="sub-%d-5">
+&nbsp;&nbsp;section 6
+</div>
+<div id="sub-%d-summary" class="bg-purple-900 rounded-lg p-3 hidden">
+</div>
+</div>`
+		div := Document.NewTag("div", fmt.Sprintf(inside, i+1,
+			i, i, i, i, i, i, i))
+		div.Set("id", fmt.Sprintf("minute-%d", i))
+		canvas.AppendChild(div.JValue)
+	}
+	canvas.RemoveClass("hidden")
 }
 
 func PollForUpdates() {
