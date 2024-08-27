fac4222992c4189b92f4cff23ee84893b5093815
Andrew Arrow <oneone@gmail.com>
2024-08-27 16:28:17 -0700

    improved logging

--- video/crop.go
+++ video/crop.go
@@ -1,4 +1,17 @@
 package video
 
+import (
+	"os/exec"
+)
+
 func RemoveBottom(id string) {
+	//ffmpeg -i input.mp4 -vf "crop=in_w:in_h-110:0:0" -c:a copy output.mp4
+	output := id + "_remove_bottom.mp4"
+	cmd := exec.Command("ffmpeg",
+		"-i", id,
+		"-vf", "crop=in_w:in_h-110:0:0", "-c:a", "copy",
+		"-y",
+		output)
+	cmd.CombinedOutput()
+
 }
