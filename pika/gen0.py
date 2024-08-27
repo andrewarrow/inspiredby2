4592c42ede7d4fb2b574b3d329a507b466a2cdb9
Andrew Arrow <oneone@gmail.com>
2024-08-27 11:10:31 -0700

    implemented new design

--- pika/gen0.py
+++ pika/gen0.py
@@ -29,11 +29,9 @@ data = (
     b'-----------------------------266926460920144731353527800262\r\n'
     b'Content-Disposition: form-data; name="styleId"\r\n\r\n\r\n'
     b'-----------------------------266926460920144731353527800262\r\n'
-    b'Content-Disposition: form-data; name="promptText"\r\n\r\nenter a parasympathetic state\r\n'
+    b'Content-Disposition: form-data; name="promptText"\r\n\r\ncollaborating with athletic trainers to analyze data\r\n'
     b'-----------------------------266926460920144731353527800262\r\n'
-    b'Content-Disposition: form-data; name="video"\r\n\r\nhttps://cdn.pika.art/v1/cdea5d77-df46-49cd-aef5-d15e32bec200/enter_a_parasympathetic_state_seed8824891511393439.mp4\r\n'
-    b'-----------------------------266926460920144731353527800262\r\n'
-    b'Content-Disposition: form-data; name="options"\r\n\r\n{"frameRate":24,"parameters":{"guidanceScale":12,"motion":1},"camera":{"zoom":null,"pan":null,"tilt":null,"rotate":null},"extend":true}\r\n'
+    b'Content-Disposition: form-data; name="options"\r\n\r\n{"frameRate":24,"parameters":{"guidanceScale":12,"motion":1},"camera":{"zoom":null,"pan":null,"tilt":null,"rotate":null},"extend":false}\r\n'
     b'-----------------------------266926460920144731353527800262\r\n'
     b'Content-Disposition: form-data; name="userId"\r\n\r\n8d46d0a2-7e9a-46d8-9796-f0458ccb4171\r\n'
     b'-----------------------------266926460920144731353527800262--\r\n'
@@ -42,4 +40,6 @@ data = (
 response = requests.post(url, headers=headers, data=data)
 
 print(response.text)
+
+
 
