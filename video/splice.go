c9ff828c2f3e1d61e45a7afd20e4cbbf2f8c15a9
Andrew Arrow <oneone@gmail.com>
2024-08-28 06:23:17 -0700

    added code comments

--- video/splice.go
+++ video/splice.go
@@ -3,6 +3,8 @@ package video
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 func Splice(dir string) {
@@ -11,14 +13,19 @@ func Splice(dir string) {
 		fmt.Println(err)
 	}
 
-	for i, file := range files {
+	for _, file := range files {
 		if !file.IsDir() {
 			if file.Name() == ".DS_Store" {
 				continue
 			}
 			//videoPath := filepath.Join(dir, file.Name())
 			name := file.Name()
-			fmt.Println(i, name)
+			tokens := strings.Split(name, ".")
+			num := tokens[0]
+			numInt, _ := strconv.Atoi(num)
+			if numInt%2 == 0 && numInt > 0 {
+				fmt.Println(num)
+			}
 		}
 	}
 }
