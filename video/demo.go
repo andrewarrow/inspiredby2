28f0856561b4291fcae523b25eff5b2582c6c262
Andrew Arrow <oneone@gmail.com>
2024-08-28 16:07:30 -0700

    added error checking

--- video/demo.go
+++ video/demo.go
@@ -20,7 +20,15 @@ func Demo() {
 			if strings.HasSuffix(name, ".mp4") == false {
 				continue
 			}
-			fmt.Println(name)
+			tokens := strings.Split(name, "_")
+			id := tokens[0]
+			fmt.Println(id)
+			file1 := dir + "/" + name
+			file2 := dir + "/foo/three_fixed_" + id + "_poster.jpg.mp4"
+			file3 := dir + "/foo3/" + id + ".mp4"
+			CombineTwoFilesOld(file1, file2, file3)
 		}
 	}
 }
+
+// three_fixed_31a5d07b-b9d8-46cb-a0fe-2f7058ee03aa_poster.jpg.mp4
