3c2ad35995aeb22b3808a1305f3e96274f7501e5
Andrew Arrow <oneone@gmail.com>
2024-08-31 14:35:38 -0700

    improved logging

--- video/render.go
+++ video/render.go
@@ -3,12 +3,13 @@ package video
 import (
 	"fmt"
 	"inspiredby2/util"
+	"time"
 
 	"github.com/andrewarrow/feedback/router"
 )
 
-func Render(c *router.Context, id int) {
-	if id == 1 {
+func Render(c *router.Context, id string) {
+	if id == "1" {
 		items := c.FreeFormSelect("select * from link_sections order by minute,sub limit 1000")
 		for i, item := range items {
 			poster, _ := item["video_poster"].(string)
@@ -17,7 +18,18 @@ func Render(c *router.Context, id int) {
 				continue
 			}
 			util.Download("posters", item["guid"].(string), poster)
+			time.Sleep(time.Second * 1)
 		}
-	} else if id == 2 {
+	} else if id == "2" {
+		items := c.FreeFormSelect("select * from link_sections order by minute,sub limit 1000")
+		for i, item := range items {
+			url, _ := item["video_url"].(string)
+			if url == "" {
+				continue
+			}
+			fmt.Println(i, url)
+			util.Download("data2", item["guid"].(string), url)
+			time.Sleep(time.Second * 1)
+		}
 	}
 }
