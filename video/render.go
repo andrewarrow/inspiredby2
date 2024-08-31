ac931d4885c091552600c2992715582ec6c5ed42
Andrew Arrow <oneone@gmail.com>
2024-08-31 15:00:26 -0700

    resolved caching issue

--- video/render.go
+++ video/render.go
@@ -3,6 +3,7 @@ package video
 import (
 	"fmt"
 	"inspiredby2/util"
+	"os/exec"
 	"time"
 
 	"github.com/andrewarrow/feedback/router"
@@ -31,5 +32,24 @@ func Render(c *router.Context, id string) {
 			util.Download("data2", item["guid"].(string), url)
 			time.Sleep(time.Second * 1)
 		}
+	} else if id == "3" {
+		items := c.FreeFormSelect("select * from link_sections order by minute,sub limit 1000")
+		for i, item := range items {
+			guid, _ := item["guid"].(string)
+			// make :q
+			make12Seconds(guid)
+
+		}
 	}
+}
+
+func make12Seconds(guid string) {
+	//ffmpeg -loop 1 -i ffebc79d-f388-0686-5fbc-de4e98ed4d16_poster.jpg_720.jpg -c:v libx264 -t 12 -pix_fmt yuv420p -vf "scale=1920:1080" output_video.mp4
+	cmd := exec.Command("ffmpeg", "-loop", "1", "-i",
+		"posters/"+guid+"_poster.jpg_720.jpg",
+		"-c:v", "libx264", "-t", "12",
+		"-pix_fmt", "yuv420p", "-vf", "scale=1280:720", "-y", guid+".mp4")
+	cmd.CombinedOutput()
+	//b, err := cmd.CombinedOutput()
+	//fmt.Println(string(b), err)
 }
