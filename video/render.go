8026035865bb8a520cd2291e5bf906eb2e040ae7
Andrew Arrow <oneone@gmail.com>
2024-08-31 15:09:38 -0700

    fixed memory fragmentation issue

--- video/render.go
+++ video/render.go
@@ -46,7 +46,7 @@ func Render(c *router.Context, id string) {
 			guid, _ := item["guid"].(string)
 
 			file1 := fmt.Sprintf("posters/%s.mp4", guid)
-			file2 := fmt.SPrintf("data2/%s.mp4", guid)
+			file2 := fmt.Sprintf("data2/%s.mp4", guid)
 			file3 := fmt.Sprintf("data3/%03d.mp4", i)
 			CombineTwoFilesWithBox(file1, file2, file3)
 		}
