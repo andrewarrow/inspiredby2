e6cfe4916833d391c3e7d9dd62efe7362e3a6f8b
Andrew Arrow <oneone@gmail.com>
2024-09-02 14:35:06 -0700

    updated configuration settings

--- video/render.go
+++ video/render.go
@@ -24,14 +24,18 @@ func Render(c *router.Context, id string) {
 	} else if id == "2" {
 		items := c.FreeFormSelect("select * from link_sections where meta!=90 and minute < 2 order by minute,sub limit 1000")
 		count := 0
+		other := 1
 		for _, item := range items {
 			url, _ := item["video_url"].(string)
 			if url == "" {
 				continue
 			}
-			fmt.Println(count, fmt.Sprintf("%03d", count), url)
-			util.Download("data3", fmt.Sprintf("%03d", count), url)
+			name := fmt.Sprintf("%03d", count)
+			fmt.Println(count, name, url)
+			util.Download("data3", name, url)
+			copyFile12("data2/"+name+".mp4", fmt.Sprintf("data3/%03d.mp4", other))
 			count += 2
+			other += 2
 			time.Sleep(time.Second * 1)
 		}
 	} else if id == "3" {
